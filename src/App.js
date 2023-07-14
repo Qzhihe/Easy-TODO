50f3cee10b1b98bbbcd14e3ef48faa2264d68048
Ra-Liz <2879055132@qq.com>
2023-07-14 22:08:34 +0800

    feat: 新增了welcome/signin/signup页面组件

--- src/App.js
+++ src/App.js
@@ -2,19 +2,26 @@ import { Fragment } from 'react';
 
 import GlobalStyle from './GlobalStyle';
 
-import Layout from './components/Layout';
-import Navbar from './components/Navbar';
-import Container from './components/Container';
+// import Layout from './components/Layout';
+// import Navbar from './components/Navbar';
+// import Container from './components/Container';
+// import Welcome from './components/Welcome';
+// import SignInSide from './components/SignInSide';
+import SignUp from './components/SignUp';
 
 function App() {
     return (
         <Fragment>
             <GlobalStyle />
 
-            <Layout>
+            {/* <Layout>
                 <Navbar></Navbar>
                 <Container></Container>
             </Layout>
+
+            <Welcome /> */}
+            {/* <SignInSide /> */}
+            <SignUp />
         </Fragment>
     );
 }
